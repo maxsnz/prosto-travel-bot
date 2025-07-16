b106034716b9ccb9005897568cd159344bc575c6
Maksim Vorozhtsov <gmaxsnz@gmail.com>
2025-07-16 17:54:13 +0300

    restart button

--- src/fsm/actions/done.ts
+++ src/fsm/actions/done.ts
@@ -2,6 +2,8 @@ import { fsmStore } from "../store";
 import { FSMAction } from "../types";
 import env from "../../config/env";
 import { hideKeyboard } from "../../utils/hideKeyboard";
+import { Markup } from "telegraf";
+import { FSM } from "..";
 
 export const done: FSMAction = {
   onEnter: async (chatId, ctx) => {
@@ -9,23 +11,31 @@ export const done: FSMAction = {
   },
   onAction: async (chatId, ctx, action) => {
     if (action === "get_guide") {
-      // Answer callback query to remove loading indicator
       await ctx.answerCbQuery();
       await hideKeyboard(ctx);
 
       const state = await fsmStore.get(chatId);
       if (state?.guideHash) {
-        // TODO: link change id to hash
         await ctx.reply(`🔗 ${env.STRAPI_HOST}/guide/${state.guideHash}`);
         await ctx.reply(
-          "Ты можешь сохранить его, распечатать или открыть в любой момент. Приятного путешествия! 🌍"
+          "Ты можешь сохранить его, распечатать или открыть в любой момент. Приятного путешествия! 🌍",
+          Markup.inlineKeyboard([
+            Markup.button.callback("🔄 Начать сначала", "start_over"),
+          ])
         );
       } else {
         await ctx.reply(
-          "❌ Извините, гид не найден. Попробуйте создать новый гид."
+          "❌ Извините, гид не найден. Попробуйте создать новый гид.",
+          Markup.inlineKeyboard([
+            Markup.button.callback("🔄 Начать сначала", "start_over"),
+          ])
         );
       }
-      await fsmStore.update(chatId, { step: "start" });
+    } else if (action === "start_over") {
+      await ctx.answerCbQuery();
+      await hideKeyboard(ctx);
+
+      await FSM["start"].onEnter(chatId, ctx);
     }
   },
 };
